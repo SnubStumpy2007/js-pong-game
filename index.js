f0527cfcc8e55f75f42dee881c5418d02b42c858
SnubStumpy2007 <joshua192007@gmail.com>
2023-12-29 17:49:45 -0500

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+// initial variables
+let canvas = document.getElementById("board");
+let ctx = canvas.getContext("2d");
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 4;
+let dy = -4;
+let ballRadius = 10;
